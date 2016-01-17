d3fc7fac8b0e83211da634f2ce103c4ee6b1b062
smokeetehad <mohammadrezanaghdpour@gmail.com>
2016-01-17 10:30:12 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,15 +228,15 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {gunners},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
   print ('saved config into ./data/config.lua')
 end
 
-function on_our_id (id)
-  our_id = id
+function on_gunners (id)
+  gunners = id
 end
 
 function on_user_update (user, what)
